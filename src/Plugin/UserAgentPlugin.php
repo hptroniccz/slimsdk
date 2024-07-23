62c22b9c1c492e34ded3b0036db2619230b260a3
David Kaprál <david@kapral.eu>
2024-07-23 15:59:23 +0200

    Doplnění hlavičky X-User-Agent do requestu

---
+++ src/Plugin/UserAgentPlugin.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Datart\Sdk\Plugin;
+
+use Http\Client\Common\Plugin;
+use Http\Promise\Promise;
+use Nette\Http\IRequest;
+use Psr\Http\Message\RequestInterface;
+
+class UserAgentPlugin implements Plugin
+{
+    private IRequest $request;
+
+    public function __construct(IRequest $request)
+    {
+        $this->request = $request;
+    }
+
+    public function handleRequest(RequestInterface $request, callable $next, callable $first): Promise // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter
+    {
+        $request = $request->withHeader('X-User-Agent', $this->request->getHeader('User-Agent'));
+
+        return $next($request);
+    }
+}
